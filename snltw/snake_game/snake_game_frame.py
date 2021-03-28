23a58ffea7df8db798fa5ff3bcd5826d373570b5
Ninh Giang <ninhgiang1502@gmail.com>
2021-03-28 15:58:42 +0700

    lesson 7

---
+++ snltw/snake_game/snake_game_frame.py
@@ -0,0 +1,48 @@
+﻿import pygame, sys, time, random
+# Đặt độ khó
+
+# Kích thước cửa sổ
+
+# Khởi tạo cửa sổ
+
+# Khai báo màu (R, G, B)
+
+# Khai báo đồng hồ FPS (frames per second)
+
+# Hàm lấy vị trí ngẫu nhiên trên màn hình
+
+# Class con rắn
+
+# Khai báo các biến trong trò chơi
+
+# Hàm tạo thức ăn
+
+# Hàm in game over
+
+# Hàm in điểm
+
+# Hàm main
+
+            # Kiểm tra nhấn phím
+
+                # W -> Up; S -> Down; A -> Left; D -> Right
+
+                # Esc -> Thoát game
+
+        # Chọn hướng đi (trừ hướng ngược với hướng đang đi chuyển)
+
+        # Thêm cái đầu
+
+        # Update đồ ăn
+
+        # Kiểm tra có ăn được không
+
+        # In điểm
+
+        # Kiểm tra điều kiện thua
+
+        # Refresh màn hình game
+
+        # Đếm theo đồng hồ
+
+# Gọi hàm main
